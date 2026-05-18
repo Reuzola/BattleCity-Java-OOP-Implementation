770d96ec05a53a9ff95147bc985d09ff69963f32
Reuzola <aliozuer@gmail.com>
2026-05-18 12:31:53 +0300

    Enemy tank shooting mechanic added

--- model/tanks/EnemyTank.java
+++ model/tanks/EnemyTank.java
@@ -6,12 +6,17 @@ import model.blocks.Block;
 
 public abstract class EnemyTank extends Tank {
 
+   private static final int FIRE_COOLDOWN_FRAMES = 60; // Equals bullet per 2 seconds. 4 times slower than player bullets
+   private int fireCooldown;
+
    public EnemyTank(int x, int y, int health, int speed) {
       super(x, y, health, speed, Direction.DOWN);
    }
 
    @Override
-   public Bullet act(ArrayList<Block> blocks){ // Basic enemy tank AI implementation
+   public Bullet act(ArrayList<Block> blocks) { // Basic enemy tank AI implementation
+      if(fireCooldown > 0) fireCooldown--; // Decreasing cooldown every tick
+
       boolean isMoved = move(blocks); // isMoved stores the information that tank is moved or not
 
       if(!isMoved) { // If enemy tank cannot move, Immediately turns randomly
@@ -20,11 +25,15 @@ public abstract class EnemyTank extends Tank {
          setDirection(d[randomIndex]);
       }
 
-      if(Math.random() < 0.02) { // 2% chance to turn every tick(~0.6 turn per second)
+      if(Math.random() < 0.02) { // %2 chance to turn every tick(~0.6 turn per second)
          Direction[] d = Direction.values();
          int randomIndex = (int)(Math.random() * d.length);
          setDirection(d[randomIndex]);
       }
-      return null;
+      
+      if(fireCooldown == 0 && Math.random() < 0.02) { // %2 chance to shoot every tick AND waits for cooldown to shoot again
+         fireCooldown = FIRE_COOLDOWN_FRAMES;
+         return fire();
+      } else return null;
    }
 }
